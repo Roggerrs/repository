1781911b35d55205740eb09600961172e82f16ad
Roger <Roger@PC_Da_Dignidade>
2024-12-05 10:15:15 -0300

    05 12 2024

--- exercicios/src/streams/ImprimirObjetos.java
+++ exercicios/src/streams/ImprimirObjetos.java
@@ -7,24 +7,25 @@ import java.util.stream.Stream;
 
 public class ImprimirObjetos {
 
+	
 	public static void main(String[] args) {
 		
-		List<String> aprovados = Arrays.asList("Lu", "Gui", "Luca", "Ana");
- 
-			System.out.println("usado o foreach...");
-			for(String nome: aprovados) {
-				System.out.println(nome);
-			}
-			
-			System.out.println("\nUsando Iterator...");
-			Iterator<String> iterator = aprovados.iterator();
-	while(iterator.hasNext()) {
-		System.out.println(iterator.next());
+		List<String> aprovados =  Arrays.asList("Lu", "Gui", "Luca", "Ana");
+		
+		System.out.println("usado o foreach...");
+		for(String nome: aprovados) {
+			System.out.println(nome);
+		}
+		
+		System.out.println("\nUsando Iterator...");
+		Iterator<String> iterator = aprovados.iterator();
+		while(iterator.hasNext()) {
+			System.out.println(iterator.next());
+		}
+		
+		System.out.println("\nUsando Strem...");
+		Stream<String> stream = aprovados.stream();
+		stream.forEach(System.out::println); // Laço interno!!!
 	}
 	
-	System.out.println("\nUsando Stream...");
-	Stream<String> stream = aprovados.stream();
-	stream.forEach(System.out::println); // Laço interno!!!
-	
-	}
 }
--- exercicios/src/streams/TesteGit.java
+++ exercicios/src/streams/TesteGit.java
@@ -4,7 +4,9 @@ public class TesteGit {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Hello world");
+		System.out.println("Hello guys");
+		
 	}
 
 }
+
